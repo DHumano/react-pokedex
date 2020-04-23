5cfefa5042222282771407761da6466eb464923b
dario <darioh.dev@gmail.com>
2020-04-23 13:43:56 -0300

    Updates

---
+++ precache-manifest.a77773ed6f9cf90a9f20dce49656bb19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5891fcc6dbd49008b459af26a935841e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a886d6580f2a5a5af79",
+    "url": "/static/css/main.8c177084.chunk.css"
+  },
+  {
+    "revision": "a9d04cc22305c36175fe",
+    "url": "/static/js/2.16f36787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16f36787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a886d6580f2a5a5af79",
+    "url": "/static/js/main.d87188b8.chunk.js"
+  },
+  {
+    "revision": "213b024422a8e31df484",
+    "url": "/static/js/runtime-main.9afaafa4.js"
+  }
+]);
